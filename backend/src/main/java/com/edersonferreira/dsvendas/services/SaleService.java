b1a948ff2e452b37694392ec86a6deed8b07d31e
Ederson Ferreira <ederson.dev@gmail.com>
2021-05-07 00:37:55 -0300

    Pagination

---
+++ backend/src/main/java/com/edersonferreira/dsvendas/services/SaleService.java
@@ -0,0 +1,29 @@
+package com.edersonferreira.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.edersonferreira.dsvendas.dto.SaleDTO;
+import com.edersonferreira.dsvendas.entities.Sale;
+import com.edersonferreira.dsvendas.repositories.SaleRepository;
+import com.edersonferreira.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+}
